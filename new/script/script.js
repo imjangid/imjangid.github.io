5850f73bb71c71b06deebea1d61068c71b4a4e8d
Ashvini Kumar Jangid <ashvinijangid@gmail.com>
2018-06-26 04:33:45 +0530

    new profile

---
+++ new/script/script.js
@@ -0,0 +1,17 @@
+//
+//
+//
+// $('.python').hover(function () {
+//     $('#services').css('background','url("files/coding.jpg") no-repeat').css('background-size','cover');
+// },function () {
+//     $('#services').css('background','None');
+// });
+//
+//
+// $('.web').hover(function () {
+//     $('#services').css('background','url("files/web_developer.jpg") no-repeat').css('background-size','cover');
+// },function () {
+//     $('#services').css('background','None');
+// });
+//
+//
